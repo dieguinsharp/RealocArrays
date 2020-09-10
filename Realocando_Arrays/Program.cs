9a358ecb4ff8d98d1cdd8d956f72fa00fd51c713
Diego <diegoomaispika@outlook.com>
2020-09-09 23:25:00 -0300

    Realocando tamanho de arrays

---
+++ Realocando_Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realocando_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array inicializado com apenas uma posição.
+            int[] test = new int[0];
+
+            Console.Write("Deseja executar o loop de testes quantas vezes?");
+            int qtd = Convert.ToInt32(Console.ReadLine());
+
+            for (int x = 0; x < qtd; x++)
+            {
+                test = Realoc.Size(ref test, x);
+                Console.WriteLine("Posição: " + x);
+                Console.WriteLine("Tamanho da array: " + test.Length);
+            }
+
+            foreach (int i in test)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Realocando_Arrays/Realoc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Realocando_Arrays
+{
+    class Realoc
+    {
+        public static int[] Size(ref int[] OldArray, int value)
+        {
+            int size_array = OldArray.Length + 1;
+
+            int[] new_array = new int[size_array];
+
+            for (int x = 0; x < OldArray.Length; x++)
+            {
+                if(x == (OldArray.Length - 1))
+                {
+                    new_array[x] = value;
+                }else
+                {
+                    new_array[x] = OldArray[x];
+                }               
+            }
+
+            return new_array;
+            
+        }
+    }
+}
